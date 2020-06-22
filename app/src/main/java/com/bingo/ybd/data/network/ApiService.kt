ceabb43098a1604cfd0ad3e0cc13dad95af67862
alex <1249110047@qq.com>
2020-06-23 01:07:46 +0800

    网络接口定义

--- app/src/main/java/com/bingo/ybd/data/network/ApiService.kt
+++ app/src/main/java/com/bingo/ybd/data/network/ApiService.kt
@@ -1,17 +1,13 @@
 package com.bingo.ybd.data.network
 
-import com.bingo.ybd.data.model.BaseResponse
-import com.bingo.ybd.data.model.UserInfo
-import kotlinx.coroutines.Deferred
-import retrofit2.Call
-import retrofit2.http.Field
-import retrofit2.http.FormUrlEncoded
-import retrofit2.http.GET
-import retrofit2.http.POST
+import com.bingo.ybd.data.model.*
+import retrofit2.http.*
 
 interface ApiService {
 //    @GET("v2/place?token=${BingoWeatherApplication.token}&lang=zh_CN")
 //    fun searchPlaces(@Query("query") query:String): Call<PlaceResponse>
+
+    /* 用户模块 */
 
     @FormUrlEncoded
     @POST("user/login.do")
@@ -29,8 +25,85 @@ interface ApiService {
         @Field("password") password: String
     ): BaseResponse<Any>
 
+
+    /* 药品模块 */
+
     @GET("medicine/getTop5MedList.do")
-    suspend fun getTop5MedList()
+    suspend fun getTop5MedList(): BaseResponse<List<MedBrief>>
+
+    @GET("medicine/getMedicineListByTypeId.do")
+    suspend fun getMedListByTypeId(
+        @Query("type_id") typeId: Int,
+        @Query("sort_flag") sortFlag: Int
+    ): BaseResponse<List<MedBrief>>
 
 
+    @GET("medicine/getMedicineDetail.do")
+    suspend fun getMedDetail(@Query("med_id") medId: Int): BaseResponse<MedDetail>
+
+    @GET("medicine/searchMed.do")
+    suspend fun searchMed(
+        @Query("keyword") keyword: String,
+        @Query("sort_flag") sortFlag: Int
+    ): BaseResponse<List<MedBrief>>
+
+
+    /* 订单模块 */
+
+    @FormUrlEncoded
+    @POST("order/addMedToCurOrder.do")
+    suspend fun addMedToCurOrder(
+        @Field("user_id") userId: Int,
+        @Field("med_id") medId: Int
+    ): BaseResponse<Any>
+
+
+    @GET("order/getMedListFromCurOrder.do")
+    suspend fun getMedCartList(@Field("user_id") userId: Int): BaseResponse<List<MedInOrder>>
+
+    @FormUrlEncoded
+    @POST("order/subMednum.do")
+    suspend fun subMedCount(
+        @Field("om_id") medOrderId: Int,
+        @Field("user_id") userId: Int
+    ): BaseResponse<Any>
+
+    @FormUrlEncoded
+    @POST("order/postNormalOrder.do")
+    suspend fun postOrder(
+        @Field("user_id") userId: Int,
+        @Field("user_phone") userPhone: String,
+        @Field("user_name") userName: String,
+        @Field("user_address") userAddress: String,
+        @Field("speed") speed: Int
+    ): BaseResponse<Any>
+
+    @GET("order/getStartingOrderByUserId.do")
+    suspend fun getStartingOrderList(@Query("user_id") userId: Int): BaseResponse<List<Order>>
+
+    @GET("order/getFinishedOrderByUserId.do")
+    suspend fun getFinishedOrderList(@Query("user_id") userId: Int): BaseResponse<List<Order>>
+
+
+    /* 发现模块 */
+
+    @GET("disc/getArticleList.do")
+    suspend fun getArticleList(): BaseResponse<Article>
+
+    @FormUrlEncoded
+    @POST("disc/getArticleDetail.do")
+    suspend fun getArticleDetail(@Field("id") articleId: Int): BaseResponse<Article>
+
+    @FormUrlEncoded
+    @POST("disc/getArticleComment.do")
+    suspend fun getArticleCommentList(@Field("id") articleId: Int): BaseResponse<List<Comment>>
+
+    @FormUrlEncoded
+    @POST("disc/addComment.do")
+    suspend fun addComment(
+        @Field("article_id") articleId: Int,
+        @Field("user_id") userId: Int,
+        @Field("content") content: String,
+        @Field("username") username: String
+    ): BaseResponse<List<Comment>>
 }
